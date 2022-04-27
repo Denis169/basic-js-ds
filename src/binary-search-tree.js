906b11f8b85f86ac4a788dfcd1f8d1e6c10c2494
Denis <denis6125038@gmail.com>
2022-04-27 22:30:52 +0300

    feat: binary-search

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -6,41 +6,116 @@ const { NotImplementedError } = require('../extensions/index.js');
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+class Node {
+  constructor(data) {
+    this.value = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+
 class BinarySearchTree {
+  constructor() {
+    this.myRoot = null;
+    this.hasAnswer = false;
+    this.itemFind = null;
+    this.arrMinValue = [];
+    this.arrMaxValue = [];
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.myRoot) {
+      return null;
+    }
+    return  {data: this.myRoot.value};
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    const newNode = new Node(data);
+
+    if (!this.myRoot) {
+      this.myRoot = newNode;
+      return;
+    }
+
+    let currentNode = this.myRoot;
+
+    while(currentNode) {
+      if (newNode.value < currentNode.value) {
+        if (!currentNode.left) {
+          currentNode.left = newNode;
+          return;
+        }
+        currentNode = currentNode.left;
+      } else {
+        if (!currentNode.right) {
+          currentNode.right = newNode;
+          return;
+        }
+        currentNode = currentNode.right;
+      }
+    }
+
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  preOrder(node, callback) {
+    if (!node) {
+      return;
+    }
+
+    if (callback) {
+      callback(node);
+    }
+
+    this.preOrder(node.left, callback);
+    this.preOrder(node.right, callback);
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    this.hasAnswer = false;
+    this.preOrder(this.myRoot, (node) => {
+      if (data === node.value) {
+        this.hasAnswer = true;
+      }
+    })
+    return this.hasAnswer;
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    this.preOrder(this.myRoot, (node) => {
+      if (data === node.value) {
+        this.itemFind = node.value;
+      }
+    })
+    if (!this.itemFind) {
+      return null;
+    }
+    return {data: this.itemFind};
+  }
+
+  remove(data) {
+    this.preOrder(this.myRoot, (node) => {
+      if (data === node.value) {
+        node.value = '';
+      }
+    })
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.arrMinValue =[];
+    this.preOrder(this.myRoot, (node) => {
+      this.arrMinValue.push(node.value);
+    })
+    return Math.min.apply( Math, this.arrMinValue.filter((item) => item !== '') );
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.arrMaxValue = [];
+    this.preOrder(this.myRoot, (node) => {
+      this.arrMaxValue.push(node.value);
+    })
+    return Math.max.apply( Math, this.arrMaxValue );
   }
 }
 
