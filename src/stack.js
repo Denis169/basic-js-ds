0075fce57a4df5672188c97b730b961eda22d45d
Denis <denis6125038@gmail.com>
2022-04-30 11:59:15 +0300

    feat: stack

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.arrStack = [];
+    this.lastVarible = '';
+  }
+
+  push(element) {
+    this.arrStack.push(element)
+    this.lastVarible = element;
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return Number(this.arrStack.splice(-1,1).join(''));
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.arrStack[this.arrStack.length - 1];
   }
 }
 
